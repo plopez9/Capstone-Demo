7c129af7a67dc03558651ffdcb1ccb9264868da2
plopez9 <pedrolopeziv@gmail.com>
2025-02-03 09:15:15 -0600

    Add axe accessibility testing

--- src/__tests__/Home.test.tsx
+++ src/__tests__/Home.test.tsx
@@ -2,9 +2,19 @@ import { screen, render } from "@testing-library/react";
 
 import Home from "@/app/page";
 
+const { axe, toHaveNoViolations } = require("jest-axe");
+expect.extend(toHaveNoViolations);
+
 describe("<Home />", () => {
-  test("It renders", () => {
+  test("It should render Home component", () => {
     render(<Home />);
     expect(screen.getByText("Learn")).toBeInTheDocument();
+  });
+
+  test("It should be accessible", async () => {
+    const { container } = render(<Home />);
+    const results = await axe(container);
+
+    expect(results).toHaveNoViolations;
   });
 });
